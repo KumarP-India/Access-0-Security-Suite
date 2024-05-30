d8d7b3e52411f03d5d5a6cee182cd04264ff51fa
Prabhas Kumar <kumar.prabhas.india@gmail.com>
2024-05-30 12:05:12 +0530

    Update setup.sh
    Changed the startup shell script for the new directive.

--- Encryption/setup/setup.sh
+++ Encryption/setup/setup.sh
@@ -19,23 +19,7 @@ HOMEBREW_PREFIX=$(brew --prefix)
 INCLUDE_PATH="$HOMEBREW_PREFIX/include"
 LIB_PATH="$HOMEBREW_PREFIX/lib"
 
-mkdir ../programs/
-mkdir ../programs/Decrypter
-gcc ../bin/Decrypter/Layer1.c -o ../programs/Decrypter/Layer1 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-gcc ../bin/Decrypter/Layer2.c -o ../programs/Decrypter/Layer2 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-gcc ../bin/Decrypter/Layer3-5.c -o ../programs/Decrypter/Layer3-5 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-gcc ../bin/Decrypter/Layer6.c -o ../programs/Decrypter/Layer6 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-gcc ../bin/Decrypter/Layer7.c -o ../programs/Decrypter/Layer7 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-gcc ../bin/Decrypter/main.c -o ../programs/Decrypter/main -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-
-mkdir ../programs/Encrypter
-gcc ../bin/Encrypter/Layer1.c -o ../programs/Encrypter/Layer1 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-gcc ../bin/Encrypter/Layer2.c -o ../programs/Encrypter/Layer2 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-gcc ../bin/Encrypter/Layer3-5.c -o ../programs/Encrypter/Layer3-5 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-gcc ../bin/Encrypter/Layer6.c -o ../programs/Encrypter/Layer6 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-gcc ../bin/Encrypter/Layer7.c -o ../programs/Encrypter/Layer7 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-gcc ../bin/Encrypter/main.c -o ../programs/Encrypter/main -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-
-mkdir ../programs/Key_and_Order
-gcc ../bin/Key_and_Order/Key_Generator.c -o ../programs/Key_and_Order/Layer1 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-gcc ../bin/Key_and_Order/Order_Generator.c -o ../programs/Key_and_Order/Layer2 -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
+gcc ../bin/generate_kyber_keypairs.c -o ../New_Keys -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
+gcc ../bin/encrypter.c -o ../Encrypter -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
+gcc ../bin/decrypter.c -o ../Decrypter -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
+gcc ../bin/finaliser.c -o ../Cleaner -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
