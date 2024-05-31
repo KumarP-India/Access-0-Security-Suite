02ab54f99fda3877fcc16ebccdb9c4869510cae1
Prabhas Kumar <kumar.prabhas.india@gmail.com>
2024-05-31 14:48:04 +0530

    Added more information for user

--- Encryption/setup/MacOS/Apple M chips (ARM)/setup.sh
+++ Encryption/setup/MacOS/Apple M chips (ARM)/setup.sh
@@ -9,9 +9,6 @@
 # @Project: Access-0-Security-Suite 's Encryption Version 1
 # @File: setup [shell script]
 
-# Install Open Quantum Safe (OQS) that provides a C to get implementation of Kyber and gcc to compile c
-brew install liboqs gcc
-
 # Define colors
 GREEN='\033[0;32m'
 RED='\033[0;31m'
@@ -19,9 +16,9 @@ NC='\033[0m'
 
 
 
-# Check if the system is macOS
+# Check if the system is MacOS
 if [[ "$(uname)" == "Darwin" ]]; then
-    echo -e "${GREEN}[Passed] ${NC}macOS system detected."
+    echo -e "${GREEN}[Passed] ${NC}MacOS system detected."
 
     # Check the architecture
     ARCHITECTURE=$(uname -m)
@@ -29,6 +26,11 @@ if [[ "$(uname)" == "Darwin" ]]; then
     if [[ "$ARCHITECTURE" == "arm64" ]]; then
         echo -e "${GREEN}[Passed] ${NC}Apple M chip (ARM architecture) detected."
 
+        # Install Open Quantum Safe (OQS) that provides a C to get implementation of Kyber and gcc to compile c
+        brew install liboqs gcc &&
+
+        echo -e "${GREEN}[FINISHED] ${NC}Required libraries downloaded and installed sucessfully."
+        
 
         # We will now compile all of the c files. Note as mentioned in ../../../README.md make changes to variables below if need arises.
     
@@ -37,15 +39,18 @@ if [[ "$(uname)" == "Darwin" ]]; then
         INCLUDE_PATH="$HOMEBREW_PREFIX/include"
         LIB_PATH="$HOMEBREW_PREFIX/lib"
         
-        gcc "../../../bin/MacOS (M chips)/generate_kyber_keypairs.c" -o "../../../New_Keys" -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-        gcc "../../../bin/MacOS (M chips)/encrypter.c" -o "../../../Encrypter" -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-        gcc "../../../bin/MacOS (M chips)/decrypter.c" -o "../../../Decrypter" -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
-        gcc "../../../bin/MacOS (M chips)/finalizer.c" -o "../../../Cleaner" -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto
+        gcc "../../../bin/MacOS (M chips)/generate_kyber_keypairs.c" -o "../../../New_Keys" -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto &&
+        gcc "../../../bin/MacOS (M chips)/encrypter.c" -o "../../../Encrypter" -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto &&
+        gcc "../../../bin/MacOS (M chips)/decrypter.c" -o "../../../Decrypter" -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto &&
+        gcc "../../../bin/MacOS (M chips)/finalizer.c" -o "../../../Cleaner" -I"$INCLUDE_PATH" -L"$LIB_PATH" -loqs -lcrypto &&
+
+        echo -e "${GREEN}[FINISHED] ${NC}Programs compiled; setup is finished."
+
     elif [[ "$ARCHITECTURE" == "x86_64" ]]; then
         echo -e "${RED}[FAILED] ${NC}Intel x86 processor detected."
     else
         echo -e "${RED}[Warning] ${NC}Unknown architecture: $ARCHITECTURE"
     fi
 else
-    echo -e "${RED}[Failed] ${NC}macOS system not detected."
+    echo -e "${RED}[Failed] ${NC}MacOS system not detected."
 fi
